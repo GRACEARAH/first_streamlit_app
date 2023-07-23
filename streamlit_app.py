72b738d0aeeb5d4978b0825b927cdd3825e9683a
GRACEARAH <140329124+GRACEARAH@users.noreply.github.com>
2023-07-23 22:51:32 +0800

    Update streamlit_app.py
    Typo Error

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ streamlit.title('My Parents New Healty Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlt.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Free-Range Egg')
