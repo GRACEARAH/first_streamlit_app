98dc978645ba3fd69e1a7636c7eb544358ca5dae
GRACEARAH <140329124+GRACEARAH@users.noreply.github.com>
2023-07-23 22:50:32 +0800

    Update streamlit_app.py
    Added Header
    Added Texts (3lines)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlt.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
